edc3cc6333a5957318eef847b13513ae9ff2aab0
Amatex1 <uncsnephews4@hotmail.com>
2025-11-20 17:42:33 +1030

    Update Profile.jsx

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import axios from "../../axios";
+import axios from "../axios";
 import "./profile.css";
 import EditProfileModal from "./EditProfileModal";
 import Post from "../Feed/Post";
