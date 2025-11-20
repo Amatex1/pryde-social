08666de1083d84fd9264c8519ae438904a4324af
Amatex1 <uncsnephews4@hotmail.com>
2025-11-20 17:26:09 +1030

    Update Account.jsx

--- client/src/components/Account.jsx
+++ client/src/components/Account.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import Profile from "./Profile/Profile";
+import Profile from "./Profile";
 
 const Account = () => {
     return (
